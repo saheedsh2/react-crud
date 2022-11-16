ea950498ac42bb8239d965f2999687a4a49cd495
saheed <saheedsh2@gmail.com>
2022-11-16 15:14:05 +0100

    website updated

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,19 +1,21 @@
+import {useState} from 'react';
 const Home = () => {
-    const handleClick = () =>{
-        console.log('hello ninjas');
-    }
 
-    const handleClickAgain = (name) => {
-        console.log('hello ' + name);
+const [blogs, setBlogs] = useState([
+    {title: "My Website", body:"Welcome to my website...", author: 'saheed', id: 1},
+    {title: "About", body:"About My Website...", author: 'shittu', id: 2},
+    {title: "Contact Us", body:"Contact Me...", author: 'luigi', id: 3},
 
-    }
+]);
     return ( 
         <div className="home">
-            <h2>Home Page</h2>
-            <button onClick={handleClick}>Click</button>
-            <button  onClick={() => {handleClickAgain('mario')}}>Click Me Again</button>
-
-
+         {blogs.map((blog) => (
+         <div className='blog-preview' key={blog.id}>
+            <h2>{ blog.title }</h2>
+            <p>Written by {blog.author}</p>
+         </div>
+         ))}
+            
         </div>
      );
 }
