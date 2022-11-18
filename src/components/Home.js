1e43974018cfff4787887d545dd6e66932a88d39
saheed <saheedsh2@gmail.com>
2022-11-18 10:51:37 +0100

    website updated

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,26 +1,32 @@
-import {useState} from 'react';
+import {useState, useEffect} from 'react';
 import BlogList from './BlogList';
 const Home = () => {
 
-const [blogs, setBlogs] = useState([
-    {title: "My Website", body:"Welcome to my website...", author: 'saheed', id: 1},
-    {title: "About", body:"About My Website...", author: 'shittu', id: 2},
-    {title: "Contact Us", body:"Contact Me...", author: 'luigi', id: 3},
+const [blogs, setBlogs] = useState(null);
+const [isPending, setIsPending] = useState(true);
 
-]);
 
-const handleDelete = (id) => {
-    const newBlogs = blogs.filter(blog => blog.id !== id);
-    setBlogs(newBlogs);
-}
+useEffect(() => {
+  setTimeout(() => {
+    fetch('http://localhost:8000/blogs')
+    .then(res => {
+     return res.json();
+    })
+    .then(data => {
+     setBlogs(data);
+     setIsPending(false);
+    })
+  }, 1000)
+
+},[])
 
 
 
 
     return ( 
         <div className="home">
-            <BlogList blogs={blogs} title="All Blogs !!" handleDelete={handleDelete}/>
-
+            {isPending && <h1>Loading......</h1>}
+            {blogs && <BlogList blogs={blogs} title="All Blogs !!"/>}
             
         </div>
      );
